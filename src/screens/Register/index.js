d059d88c001365f57ef84610bba1ef1cca083d6f
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2021-07-06 09:55:30 +0430

    implement login screen

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,12 +1,69 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {LOGIN} from '../../constants/routeNames';
+
+// components
+import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native';
+import Button from '../../components/Button';
+import Container from '../../components/Container';
+import Input from '../../components/Input';
+import colors from '../../assets/theme/colors';
 
 const Register = () => {
+  const [text, onChangeText] = React.useState('');
+  const {navigate} = useNavigation();
   return (
-    <View>
-      <Text>Hello from Register Screen</Text>
-    </View>
+    <Container>
+      <Image
+        source={require('../../assets/images/logo.png')}
+        width={70}
+        height={70}
+        style={styles.logoImage}
+      />
+      <Input
+        label="Username"
+        placeholder="Enter Username"
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Input
+        label="Password"
+        onChangeText={onChangeText}
+        iconPosition="right"
+        secureTextEntryds
+        value={text}
+        placeholder="Enter Your Password"
+      />
+      <Button title="Submit" color="primary" />
+
+      <View style={styles.registerSection}>
+        <Text style={styles.infoText}>Already have an account?</Text>
+        <TouchableOpacity onPress={() => navigate(LOGIN)}>
+          <Text style={styles.linkButton}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  logoImage: {
+    height: 180,
+    width: 180,
+    alignSelf: 'center',
+    marginTop: 50,
+  },
+  registerSection: {
+    flexDirection: 'row',
+    paddingTop: 20,
+  },
+  infoText: {
+    paddingRight: 5,
+    fontSize: 15,
+  },
+  linkButton: {
+    color: colors.primary,
+  },
+});
 
 export default Register;
